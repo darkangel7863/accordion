e2b29b9624771569f8defa35d3c7d2e23bee3887
Mohamed El Sayed Ahmed Ismail <darkangel7863@gmail.com>
2024-06-30 10:21:03 +0300

    Rebuilding the component

--- src/App.js
+++ src/App.js
@@ -25,35 +25,26 @@ export default function App() {
 }
 
 function Accordion({ data }) {
-  const [curOpen, setCurOpen] = useState(null);
   return (
     <div className="accordion">
       {data.map((el, i) => (
-        <AccordionItem
-          curOpen={curOpen}
-          onOpen={setCurOpen}
-          title={el.title}
-          num={i}
-          key={i}
-        >
-          {el.text}
-        </AccordionItem>
+        <AccordionItem title={el.title} text={el.text} num={i} key={el.title} />
       ))}
     </div>
   );
 }
 
-function AccordionItem({ num, title, curOpen, onOpen, children }) {
-  const isOpen = num === curOpen;
+function AccordionItem({ num, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
   function handleToggle() {
-    onOpen(isOpen ? null : num);
+    setIsOpen(isOpen => !isOpen);
   }
   return (
     <div className={`item ${isOpen ? 'open' : ''}`} onClick={handleToggle}>
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="text">{title}</p>
       <p className="icon">{isOpen ? '-' : '+'}</p>
-      {isOpen && <div className="content-box">{children}</div>}
+      {isOpen && <div className="content-box">{text}</div>}
     </div>
   );
 }
